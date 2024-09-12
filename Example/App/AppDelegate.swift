23f3ab36d5ed11ff03555d4bfa9ce0dce2ed1645
hexueshi <hexueshi@ijiami.cn>
2024-09-12 17:14:52 +0800

    1.指定链接库顺序

---
+++ Example/App/AppDelegate.swift
@@ -0,0 +1,116 @@
+//
+//  AppDelegate.swift
+//  TUICallKitApp
+//
+//  Created by adams on 2021/5/7.
+//  Copyright © 2021 Tencent. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+import ImSDK_Plus
+
+#if canImport(TUICallKit_Swift)
+import TUICallKit_Swift
+#elseif canImport(TUICallKit)
+import TUICallKit
+#endif
+
+#if canImport(TXLiteAVSDK_TRTC)
+import TXLiteAVSDK_TRTC
+#elseif canImport(TXLiteAVSDK_Professional)
+import TXLiteAVSDK_Professional
+#endif
+
+/// You need to register a developer certificate with Apple, download and generate the certificate (P12 file) in their developer accounts, and upload the generated P12 file to the Tencent certificate console.
+/// The console will automatically generate a certificate ID and pass it to the `businessID` parameter.
+#if DEBUG
+let businessID: Int32 = 0
+#else
+let businessID: Int32 = 0
+#endif
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var deviceToken: Data? = nil
+    var unreadNumber: UInt64 = 0
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        /// Configuration Apple Push Notification Service (APNs)
+        registerRemoteNotifications(with: application)
+        V2TIMManager.sharedInstance().setAPNSListener(self)
+        V2TIMManager.sharedInstance().addConversationListener(listener: self)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(registerIfLoggedIn(_:)),
+                                               name: Notification.Name("TUILoginSuccessNotification"),
+                                               object: nil)
+        
+        return true
+    }
+    
+}
+
+// MARK: - Configuration Apple Push Notification Service (APNs)
+
+extension AppDelegate: V2TIMConversationListener, V2TIMAPNSListener {
+    
+    // Register Remote Notifications
+    func registerRemoteNotifications(with application: UIApplication) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { (isGrand, error) in
+            if let error = error {
+                debugPrint("Error requesting authorization for remote notifications: \(error.localizedDescription)")
+            } else {
+                DispatchQueue.main.async {
+                    application.registerForRemoteNotifications()
+                }
+            }
+        }
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("Register device token success")
+        self.deviceToken = deviceToken
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        debugPrint("Failed to register for remote notifications: \(error.localizedDescription)")
+    }
+    
+    @objc func registerIfLoggedIn(_ notification: Notification) {
+        DispatchQueue.main.async {
+            TUICallKit.createInstance().enableFloatWindow(enable: true)
+#if canImport(TUICallKit_Swift)
+            TUICallKit.createInstance().enableVirtualBackground(enable: true)
+            TUICallKit.createInstance().enableIncomingBanner(enable: true)
+#endif
+        }
+        
+        let config = V2TIMAPNSConfig()
+        config.token = deviceToken
+        config.businessID = businessID
+        V2TIMManager.sharedInstance()?.setAPNS(config, succ: {
+            debugPrint("setAPNS success")
+        }, fail: { code, msg in
+            debugPrint("setAPNS error code:\(code), error: \(msg ?? "nil")")
+        })
+    }
+    
+    // When the unread message count changes, save the new unread message count
+    func onTotalUnreadMessageCountChanged(_ totalUnreadCount: UInt64) {
+        unreadNumber = totalUnreadCount;
+    }
+    
+    // The app goes to the background and reports the custom unread message count
+    func onSetAPPUnreadCount() -> UInt32 {
+        // 1. Get the custom badge number
+        var customBadgeNumber : UInt64 = 0
+        // 2. Add the unread message count of IM
+        customBadgeNumber += unreadNumber;
+        // 3. Use the IM SDK to report to the IM server
+        return UInt32(customBadgeNumber);
+    }
+    
+}
---
+++ Example/App/Call/SingleCallViewController.swift
@@ -0,0 +1,284 @@
+//
+//  SingleCallViewController.swift
+//  TUICallKitApp
+//
+//  Created by vincepzhang on 2023/5/9.
+//
+
+import Foundation
+import UIKit
+import TUICore
+import TUICallEngine
+
+#if canImport(TUICallKit_Swift)
+import TUICallKit_Swift
+#elseif canImport(TUICallKit)
+import TUICallKit
+#endif
+
+public class SingleCallViewController: UIViewController, UITextFieldDelegate {
+    var callType: TUICallMediaType = .audio
+    lazy var line1View: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor(hex: "EEEEEE")
+        return view
+    }()
+    lazy var userIdContentView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    lazy var userIdTextLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor.black
+        label.text = TUICallKitAppLocalize("TUICallKitApp.Call.UserId")
+        return label
+    }()
+    lazy var calledUserIdTextField: UITextField = {
+        let textField = UITextField(frame: .zero)
+        textField.backgroundColor = UIColor.clear
+        textField.font = UIFont(name: "PingFangSC-Regular", size: 16)
+        textField.textColor = UIColor(hex: "333333")
+        textField.attributedPlaceholder = NSAttributedString(string: TUICallKitAppLocalize("TUICallKitApp.Call.InputUserId"))
+        textField.textAlignment = .right
+        textField.delegate = self
+        return textField
+    }()
+    weak var currentTextField: UITextField?
+    
+    lazy var line2View: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor(hex: "EEEEEE")
+        return view
+    }()
+    lazy var mediaTypeContentView: UIView = {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = UIColor.white
+        return view
+    }()
+    lazy var typeLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor.black
+        label.text = TUICallKitAppLocalize("TUICallKitApp.MediaType")
+        return label
+    }()
+    lazy var videoButton: RadioButton = {
+        let button = RadioButton(frame: CGRect.zero)
+        button.titleText = TUICallKitAppLocalize("TUICallKitApp.Video.call")
+        button.titleSize = 16
+        return button
+    }()
+    lazy var voiceButton: RadioButton = {
+        let button = RadioButton(frame: CGRect.zero)
+        button.titleText = TUICallKitAppLocalize("TUICallKitApp.Audio.call")
+        button.isSelected = true
+        button.titleSize = 16
+        return button
+    }()
+    lazy var buttons: [RadioButton] = {
+        let buttons = [videoButton, voiceButton]
+        return buttons
+    }()
+    lazy var callSettingsLabel: UILabel = {
+        let label = UILabel(frame: .zero)
+        label.font = UIFont.systemFont(ofSize: 16)
+        label.textColor = UIColor.blue
+        label.text = "\(TUICallKitAppLocalize("TUICallKitApp.Setting.CallSettings"))  >"
+        label.isUserInteractionEnabled = true
+        return label
+    }()
+    lazy var callButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.setTitleColor(.white, for: .normal)
+        btn.setTitle(TUICallKitAppLocalize("TUICallKitApp.Call"), for: .normal)
+        btn.adjustsImageWhenHighlighted = false
+        btn.setBackgroundImage(UIColor(hex: "006EFF")?.trans2Image(), for: .normal)
+        btn.titleLabel?.font = UIFont(name: "PingFangSC-Medium", size: 20)
+        btn.layer.shadowColor = UIColor(hex: "006EFF")?.cgColor ?? UIColor.blue.cgColor
+        btn.layer.shadowOffset = CGSize(width: 0, height: 6)
+        btn.layer.shadowRadius = 16
+        btn.layer.shadowOpacity = 0.4
+        btn.layer.masksToBounds = true
+        btn.layer.cornerRadius = 10
+        return btn
+    }()
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+        title = TUICallKitAppLocalize("TUICallKitApp.SingleCall")
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.black]
+        
+        let backButton = UIButton(type: .system)
+        backButton.setImage(UIImage(named: "callKit_back"), for: .normal)
+        backButton.addTarget(self, action: #selector(backButtonClick), for: .touchUpInside)
+        let item = UIBarButtonItem(customView: backButton)
+        item.tintColor = UIColor.black
+        navigationItem.leftBarButtonItem = item
+        
+        navigationController?.navigationBar.isHidden = false
+        
+        constructViewHierarchy()
+        activateConstraints()
+        bindInteraction()
+    }
+    
+    func constructViewHierarchy() {
+        view.addSubview(line1View)
+        
+        view.addSubview(userIdContentView)
+        userIdContentView.addSubview(calledUserIdTextField)
+        userIdContentView.addSubview(userIdTextLabel)
+        
+        view.addSubview(line2View)
+        
+        view.addSubview(mediaTypeContentView)
+        mediaTypeContentView.addSubview(typeLabel)
+        mediaTypeContentView.addSubview(videoButton)
+        mediaTypeContentView.addSubview(voiceButton)
+        
+        view.addSubview(callSettingsLabel)
+        
+        view.addSubview(callButton)
+    }
+    
+    func activateConstraints() {
+        line1View.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(100)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(10)
+        }
+        userIdContentView.snp.makeConstraints { make in
+            make.top.equalTo(line1View.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        userIdTextLabel.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalToSuperview().offset(20)
+        }
+        calledUserIdTextField.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(userIdTextLabel.snp.trailing).offset(20)
+            make.trailing.equalToSuperview().offset(-20)
+        }
+        line2View.snp.makeConstraints { make in
+            make.top.equalTo(userIdContentView.snp.bottom)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(10)
+        }
+        mediaTypeContentView.snp.makeConstraints { make in
+            make.top.equalTo(calledUserIdTextField.snp.bottom).offset(40)
+            make.leading.trailing.equalToSuperview()
+            make.width.equalToSuperview()
+            make.height.equalTo(30)
+        }
+        typeLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview().offset(20)
+        }
+        videoButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(typeLabel.snp.trailing).offset(40)
+        }
+        voiceButton.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.leading.equalTo(videoButton.snp.trailing).offset(80)
+        }
+        callSettingsLabel.snp.makeConstraints { make in
+            make.top.equalTo(mediaTypeContentView.snp.bottom).offset(50)
+            make.centerX.equalToSuperview()
+        }
+        callButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-60)
+            make.height.equalTo(60)
+            make.width.equalToSuperview().offset(-40)
+        }
+    }
+    
+    func bindInteraction() {
+        videoButton.addTarget(self, action: #selector(radioButtonTapped), for: .touchUpInside)
+        voiceButton.addTarget(self, action: #selector(radioButtonTapped), for: .touchUpInside)
+        
+        let callSettingsLabelTapGesture = UITapGestureRecognizer(target: self, action: #selector(settingButtonClick))
+        callSettingsLabel.addGestureRecognizer(callSettingsLabelTapGesture)
+        
+        callButton.addTarget(self, action: #selector(callButtonClick), for: .touchUpInside)
+    }
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        if let current = currentTextField {
+            current.resignFirstResponder()
+            currentTextField = nil
+        }
+    }
+    
+    @objc func radioButtonTapped(_ sender: RadioButton) {
+        buttons.forEach({ $0.isSelected = false})
+        sender.isSelected = true
+        if sender == videoButton {
+            callType = .video
+        } else {
+            callType = .audio
+        }
+    }
+    
+    @objc func callButtonClick() {
+        guard let userId = calledUserIdTextField.text else { return }
+        
+        if userId.isEmpty {
+            TUITool.makeToast("Please input userId & groupId ")
+            return
+        }
+        
+        let params = TUICallParams()
+        params.timeout = Int32(SettingsConfig.share.timeout)
+        params.userData = SettingsConfig.share.userData
+        params.offlinePushInfo = SettingsConfig.share.pushInfo
+        
+        let roomId = TUIRoomId()
+        roomId.intRoomId = SettingsConfig.share.intRoomId
+        roomId.strRoomId = SettingsConfig.share.strRoomId
+        params.roomId = roomId
+        
+        TUICallKit.createInstance().call(userId: userId, callMediaType: callType, params: params) {
+            
+        } fail: { code, message in
+            TUITool.makeToast("Error \(code):\(message ?? "")")
+        }
+    }
+    
+    @objc func backButtonClick() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func settingButtonClick() {
+        let settingVC = SettingsViewController()
+        settingVC.title = TUICallKitAppLocalize("TUICallKitApp.Setting.CallSettings")
+        settingVC.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(settingVC, animated: true)
+    }
+}
+
+extension SingleCallViewController {
+    public func textFieldDidBeginEditing(_ textField: UITextField) {
+        if let last = currentTextField {
+            last.resignFirstResponder()
+        }
+        currentTextField = textField
+        textField.becomeFirstResponder()
+    }
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.resignFirstResponder()
+        currentTextField = nil
+    }
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
